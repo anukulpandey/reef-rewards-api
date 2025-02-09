8242f40d2eb2febc8302ff56fe977130f7197c00
Anukul Pandey <anukul.030601@gmail.com>
2025-02-09 17:42:39 +0530

    all validators

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { WsProvider } = require('@polkadot/api');
+const {Provider} = require('@reef-chain/evm-provider');
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 const RPC_URL = "wss://rpc.reefscan.com/ws";
 
-
-async function getValidators() {
-  const provider = new WsProvider(RPC_URL);
-  const api = await ApiPromise.create({ provider });
-  return await api.query.session.validators();
+async function getProvider(){
+  const provider = new Provider({
+    provider: new WsProvider(RPC_URL)
+  });
+  await provider.api.isReadyOrError;
+  return provider.api; 
 }
 
-async function getNominatorsForValidator(validatorAddress) {
-  const provider = new WsProvider(RPC_URL);
-  const api = await ApiPromise.create({ provider });
-  const nominatorsEntries = await api.query.staking.nominators.entries();
-
-const nominatorsForValidator = nominatorsEntries
-  .filter(([_, nominatorOpt]) => {
-    if (nominatorOpt.isSome) {
-      const nominator = nominatorOpt.unwrap();
-      return nominator.targets.includes(validatorAddress);
-    }
-    return false;
-  })
-  .map(([nominatorId]) => nominatorId.toHuman());
-
-  console.log("Nominators for ",validatorAddress," :",nominatorsForValidator);
-
-  process.exit(1);
+async function getValidators() {
+  const api = await getProvider();
+  return await api.query.session.validators();
 }
 
 app.get("/validators", async (req, res) => {
     try {
-        const validators = await getValidators(RPC_URL);
+        const validators = await getValidators();
         res.json({ validators });
     } catch (error) {
         console.error("Error fetching validators:", error);
